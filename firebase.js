125a52c2d5ed45f1d91004d69d21e59aeace8eac
cullorsrb45 <cullorsrb45@yahoo.com>
2021-09-16 02:13:40 -0700

    day3

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbuh4gFWT_3o2ZF1kryqbVc8J-pl4EPec",
+    authDomain: "amzn-2-ac.firebaseapp.com",
+    projectId: "amzn-2-ac",
+    storageBucket: "amzn-2-ac.appspot.com",
+    messagingSenderId: "246635425869",
+    appId: "1:246635425869:web:c2dd05085a8419e939ec04"
+};
